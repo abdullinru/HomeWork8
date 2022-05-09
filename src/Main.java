1aabb9b6e051df7d5931070f2aa9320d46762aa2
abdullinru <abdullin17021989@gmail.com>
2022-05-10 01:55:52 +0600

    Второе задание

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-        isYearVis(1900);
+//        isYearVis(1900);
+        installApp(1,2022);
     }
     private static void isYearVis(int year) {
         if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
@@ -8,5 +11,23 @@ public class Main {
         } else {
             System.out.println(year + " - невесокосный год");
         }
+    }
+    private static void installApp(int os, int clientDeviceYear) {
+        int currentYear = LocalDate.now().getYear();
+        if (os == 0) {
+            if (clientDeviceYear < currentYear) {
+                System.out.println("Установите лайт версию приложения для IOS");
+            } else {
+                System.out.println("Установите версию приложения для IOS");
+            }
+        }
+        if (os == 1) {
+            if (clientDeviceYear < currentYear) {
+                System.out.println("Установите лайт версию приложения для Андроид");
+            } else {
+                System.out.println("Установите версию приложения для Андроид");
+            }
+        }
+
     }
 }
